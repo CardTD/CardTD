7b43b4bebb0e59b55343ce30353f6207307674b8
Escanor Targaryen <bilardellodavides@gmail.com>
2022-04-16 21:35:49 +0200

    init repo

---
+++ desktop/src/it/simone/davide/cardtd/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package it.simone.davide.cardtd;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import it.simone.davide.cardtd.CardTDGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		new Lwjgl3Application(new CardTDGame(), config);
+	}
+}
---
+++ ios/src/it/simone/davide/cardtd/IOSLauncher.java
@@ -0,0 +1,22 @@
+package it.simone.davide.cardtd;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import it.simone.davide.cardtd.CardTDGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new CardTDGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
