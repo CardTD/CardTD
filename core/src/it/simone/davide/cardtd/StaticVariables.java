da1e54e542c13d59af309bcc68b920a9a1330604
Escanor Targaryen <bilardellodavides@gmail.com>
2022-04-19 14:35:13 +0200

    Elimina variabili inutili

--- core/src/it/simone/davide/cardtd/StaticVariables.java
+++ core/src/it/simone/davide/cardtd/StaticVariables.java
@@ -10,7 +10,5 @@ public class StaticVariables {
     public static final String GAMENAME = "CardTD";
 
     public static final String MAIN_MENU_IMG = "mainmenu.png";
-    public static final String MAIN_FONT = "Rundown.ttf";
 
-    public static final int TITLE_FONT_SIZE = 150;
 }
