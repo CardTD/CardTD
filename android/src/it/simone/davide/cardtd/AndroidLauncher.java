a73ded146b730e16779b4d27ddc666bf771c54c9
SimoneGuido04 <guidosimone02@gmail.com>
2022-04-30 15:46:05 +0200

    Added Notch Support

--- android/src/it/simone/davide/cardtd/AndroidLauncher.java
+++ android/src/it/simone/davide/cardtd/AndroidLauncher.java
@@ -1,7 +1,10 @@
 package it.simone.davide.cardtd;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import android.view.Window;
+import android.view.WindowManager;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import it.simone.davide.cardtd.CardTDGame;
@@ -9,6 +12,15 @@ import it.simone.davide.cardtd.CardTDGame;
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
+
+		//Notch Eliminata
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+			Window applicationWindow = getApplicationWindow();
+			WindowManager.LayoutParams attrib = applicationWindow.getAttributes();
+			attrib.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+		}
+
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		config.useWakelock = true;
